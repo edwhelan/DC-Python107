4a3071796aff416a1f23ccfd67f3b548972bfd82
Edward Whelan <vroxing@gmail.com>
2018-09-25 18:29:04 -0400

    add detection of most titles

--- game_of_thrones.py
+++ game_of_thrones.py
@@ -1,16 +1,16 @@
 from sample import characters
 
 
-print(characters.characters[1]['died'])
+print(len(characters.characters[1]['titles']))
 
-
+#Print how many characters are dead
 dead_count = 0
 for person in characters.characters:
     if person['died'] != '':
         dead_count += 1
 
 print(dead_count)
-
+#Print how many characters start with the letter A
 how_many_start_alpha = 0
 for person in characters.characters:
     if person['name'].startswith('A'):
@@ -18,9 +18,21 @@ for person in characters.characters:
 
 print(how_many_start_alpha)
 
+
+#print how many characters start with the letter Z
 how_many_start_zebra = 0
 for person in characters.characters:
     if person['name'].startswith('Z'):
         how_many_start_zebra += 1
 
 print(how_many_start_zebra)
+
+#detect who has the most titles and print that
+current_most = ''
+the_most_titles = 0
+for person in characters.characters:
+    if len(person['titles']) > the_most_titles:
+        current_most = person['name']
+        the_most_titles = len(person['titles'])
+        
+print('%s has the most titles with %d' % (current_most, the_most_titles))
